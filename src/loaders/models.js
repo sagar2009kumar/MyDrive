f021961928583c9e5de05c0cfe45fe3929cfb81f
Sagar Kumar <sagar.kumar@ebizontek.com>
2019-09-16 20:00:13 +0530

    Fixed the issue of connection to the database

--- src/loaders/models.js
+++ src/loaders/models.js
@@ -1,20 +1,25 @@
 const mongoose = require("mongoose");
 
 const initializeMongoose = async () => {
-  mongoose
-    .connect(process.env.MONGODB_CONNECTION_URL, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true
-    })
-    .then(() => {
-      console.log("Database connected");
-    })
-    .catch(e => {
-      console.log("Unable to connect to database");
-      console.log(e);
-      throw new Error("Unable to connect to database");
-    });
+
+  try { 
+    mongoose
+      .connect(process.env.MONGODB_CONNECTION_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+      })
+      .then(() => {
+        console.log("Database connected");
+      })
+      .catch(e => {
+        console.log("Unable to connect to database");
+        throw new Error("Unable to connect to database");
+      });
+    }catch(error) {
+      console.log('Unable to connect to database' + error.message);
+      process.exit(1);
+    }
 };
 
 module.exports = initializeMongoose;
